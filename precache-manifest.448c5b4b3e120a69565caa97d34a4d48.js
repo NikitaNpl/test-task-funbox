ea083e5e7bfd8ce5551b25cc7350a8a068e0a014
NikitaNpl <totmyanin.n@bk.ru>
2020-08-24 17:08:22 +0300

    Updates

---
+++ precache-manifest.448c5b4b3e120a69565caa97d34a4d48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496249131c45921aaf36cc33a2ca7d8b",
+    "url": "/test-task-funbox/index.html"
+  },
+  {
+    "revision": "16e27adadbb9adece15d",
+    "url": "/test-task-funbox/static/css/main.939a816f.chunk.css"
+  },
+  {
+    "revision": "7d95c71015f3bbca9c4b",
+    "url": "/test-task-funbox/static/js/2.68187928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-task-funbox/static/js/2.68187928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e27adadbb9adece15d",
+    "url": "/test-task-funbox/static/js/main.6a765072.chunk.js"
+  },
+  {
+    "revision": "bdb653eaf9faf7244062",
+    "url": "/test-task-funbox/static/js/runtime-main.6647d118.js"
+  },
+  {
+    "revision": "552028970740f63a505bbc045e450699",
+    "url": "/test-task-funbox/static/media/cat.55202897.png"
+  },
+  {
+    "revision": "04ba3a002072928bb8fdcb624ebea9af",
+    "url": "/test-task-funbox/static/media/pattern.04ba3a00.png"
+  }
+]);
